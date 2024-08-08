8bcabb0c01962b388d252dd1f9c0235ef044f20a
jackfauji <jackfauji1000@gmail.com>
2024-08-08 10:17:19 +0530

    Update t1.cpp

--- t1.cpp
+++ t1.cpp
@@ -1,9 +1,11 @@
+//wap in c++ to capitalise 1st letter of each word in a string.
 #include<iostream>
 #include<cstring>
 using namespace std;
 int main()
 {
     string str;
+    cout<<"enter the string";
     getline(cin,str);
     int i=0;
     str[i]=str[i]-32;
